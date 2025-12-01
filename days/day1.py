d9b09962f7ac7307e527ca650e7e571b094394ac
Ulf <ulf.trulf@web.de>
2025-12-01 20:43:44 +0100

    add main, add day1

---
+++ days/day1.py
@@ -0,0 +1,43 @@
+import sys
+import copy
+
+def convert(s: str, n: int) -> int:
+    if s == "L":
+        return -n
+    else:
+        return n
+
+
+def part1(turns):
+    dial = 50
+    modu = 100
+    answ = 0
+    for turn in turns:
+        dial = dial + turn
+        dial = dial % modu
+        if dial == 0:
+            answ += 1
+    print(answ)
+
+def part2(turns):
+    dial = 50
+    modu = 100
+    answ = 0
+    for turn in turns:
+        if turn > 0:
+            answ += (dial + turn) // modu
+        else:
+            answ += (modu - dial - turn) // modu
+            if dial == 0:
+                answ -= 1
+
+        dial = (dial + turn) % modu
+    print(answ)
+
+data = sys.stdin.read()
+data = data.splitlines()
+turns = [ convert(line[0], int(line[1:])) for line in data ]
+part1(turns)
+part2(turns)
+
+
